36425d8fefcf5db4b7d3db1aceddcd33cf152cc8
Brian Bockelman <bbockelm@cse.unl.edu>
2020-09-13 21:35:04 -0500

    Allow scheduler to preserve worker hostnames
    This provides a configuration option allowing the scheduler to
    preserve worker hostnames (as opposed to the current & default
    behavior of resolving hostnames immediately to IP address).
    
    Preserving the hostname should convey advantages in two situations:
    
    1. Dual-network setups where some hosts have IPv4-only or IPv6-only.
       This allows the host that wants to communicate with the worker
       to determine the best network to route over (as opposed to the
       scheduler).
    2. Clusters that route TLS connections based on SNI.  If the hostname
       is kept internally, it will be pased to OpenSSL when making a
       connection.  This allows the OpenSSL client to embed the desired
       hostname in a SNI header in the TLS handshake.  If the worker is
       behind a TLS-router (not entirely uncommon in a Kubernetes setup),
       then this SNI is required to successfully route the request.

--- distributed/scheduler.py
+++ distributed/scheduler.py
@@ -171,6 +171,8 @@ UNKNOWN_TASK_DURATION = declare(
     parse_timedelta(dask.config.get("distributed.scheduler.unknown-task-duration")),
 )
 
+RESOLVE_WORKER_HOSTNAME = dask.config.get("distributed.scheduler.resolve-worker-hostname")
+
 DEFAULT_EXTENSIONS = [
     LockExtension,
     PublishExtension,
@@ -2148,7 +2150,7 @@ class Scheduler(ServerNode):
         self,
         comm=None,
         address=None,
-        resolve_address=True,
+        resolve_address=RESOLVE_WORKER_HOSTNAME,
         now=None,
         resources=None,
         host_info=None,
@@ -2224,7 +2226,7 @@ class Scheduler(ServerNode):
         keys=(),
         nthreads=None,
         name=None,
-        resolve_address=True,
+        resolve_address=RESOLVE_WORKER_HOSTNAME,
         nbytes=None,
         types=None,
         now=None,
@@ -5760,7 +5762,7 @@ class Scheduler(ServerNode):
         for resource, quantity in ws._resources.items():
             del self.resources[resource][worker]
 
-    def coerce_address(self, addr, resolve=True):
+    def coerce_address(self, addr, resolve=RESOLVE_WORKER_HOSTNAME):
         """
         Coerce possible input addresses to canonical form.
         *resolve* can be disabled for testing with fake hostnames.
